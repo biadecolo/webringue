d1aa30f4a1a181e871e507d18a0e171cf7d55c0c
DoceAzedo <me@doceazedo.com>
2026-07-19 22:50:55 -0300

    setup hono

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", serveStatic({ path: "./webringue.html" }));
+
+app.use("/assets/*", serveStatic({ root: "./" }));
+
+const port = Number(process.env.PORT) || 3000;
+
+serve({ fetch: app.fetch, port }, ({ port }) => {
+  console.log(`webringue rodando em http://localhost:${port}`);
+});
